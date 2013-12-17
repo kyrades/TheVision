4997bd85228be1f9f50307f0500b13aecad2f086
Florian <florian@adeptropolis.net>
2013-12-17 23:15:08 +0100

    Yay! First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheVision::Application.config.secret_key_base = 'c12d3dc54691d1ed5cf1f7f1aad86eb46d704fe947724ace9936d45d3ad2deca3e3d04890ce6ec5f5860df3136a89887e603dacc31fd9a63389a6a32e4ceb857'
